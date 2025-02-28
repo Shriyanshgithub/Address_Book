b6153fedf84c424978d45bfe4e56e0c056bc0b0d
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-02-28 12:35:47 +0530

    [Shriyansh Gupta] Add. add validation on fields

--- address-book-spring-project/src/main/java/com/capgemini/address_book/model/AddressBookEntity.java
+++ address-book-spring-project/src/main/java/com/capgemini/address_book/model/AddressBookEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,14 @@ public class AddressBookEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @Pattern(regexp = "^[A-Za-z0-9]+$")
     private String name;
+    @NotNull
+    @Min(value = 4,message = "minimum size of address is 4")
+    @Max(value = 7, message = "maximum size of address is 7")
     private String address;
+    @NotEmpty
     private String tittle;
 
 
